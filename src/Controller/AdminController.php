fb39c39c5415d6521796f9d8da59da9b2a4e1e5c
Richard Lucas <webmaster@applebiter.com>
2022-11-12 15:16:18 -0500

    Began work on the admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -17,7 +17,22 @@ class AdminController extends AppController
      */
     public function carrier($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $CarriersTable = $this->fetchTable("Carriers");
+        $carrier = $CarriersTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('carrier'));
     }
 
     /**
@@ -27,7 +42,26 @@ class AdminController extends AppController
      */
     public function carriers()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $CarriersTable = $this->fetchTable("Carriers");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $carriers = $this->paginate($CarriersTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('carriers'));
     }
 
     /**
@@ -39,7 +73,22 @@ class AdminController extends AppController
      */
     public function country($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $CountriesTable = $this->fetchTable("Countries");
+        $country = $CountriesTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('country'));
     }
 
     /**
@@ -49,7 +98,26 @@ class AdminController extends AppController
      */
     public function countries()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $CountriesTable = $this->fetchTable("Countries");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $countries = $this->paginate($CountriesTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('countries'));
     }
     
     /**
@@ -139,7 +207,12 @@ class AdminController extends AppController
      */
     public function logs($type = null)
     {
-
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
     }
 
     /**
@@ -151,7 +224,22 @@ class AdminController extends AppController
      */
     public function permission($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $PermissionsTable = $this->fetchTable("ResourcesRoles");
+        $permission = $PermissionsTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('permission'));
     }
 
     /**
@@ -161,7 +249,26 @@ class AdminController extends AppController
      */
     public function permissions()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $PermissionsTable = $this->fetchTable("ResourcesRoles");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $permissions = $this->paginate($PermissionsTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('permissions'));
     }
 
     /**
@@ -173,7 +280,22 @@ class AdminController extends AppController
      */
     public function resource($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $ResourcesTable = $this->fetchTable("Resources");
+        $resource = $ResourcesTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('resource'));
     }
 
     /**
@@ -183,7 +305,26 @@ class AdminController extends AppController
      */
     public function resources()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $ResourcesTable = $this->fetchTable("Resources");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $resources = $this->paginate($ResourcesTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('resources'));
     }
 
     /**
@@ -195,7 +336,22 @@ class AdminController extends AppController
      */
     public function role($id = null) 
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $RolesTable = $this->fetchTable("Roles");
+        $role = $RolesTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('role'));
     }
 
     /**
@@ -205,7 +361,26 @@ class AdminController extends AppController
      */
     public function roles()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $RolesTable = $this->fetchTable("Roles");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $roles = $this->paginate($RolesTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('roles'));
     }
 
     /**
@@ -216,7 +391,27 @@ class AdminController extends AppController
      */
     public function sessions($id = null)
     {
+        $_session = $this->request->getSession();
+        
+        if (!$_session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $SessionsTable = $this->fetchTable("Sessions");
+        $session = $id ? $SessionsTable->find()->where(['id' => $id])->first() : null;
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $sessions = $this->paginate($SessionsTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('sessions', 'session'));
     }
 
     /**
@@ -228,7 +423,22 @@ class AdminController extends AppController
      */
     public function sound($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $SoundsTable = $this->fetchTable("Sounds");
+        $sound = $SoundsTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('sound'));
     }
 
     /**
@@ -238,7 +448,26 @@ class AdminController extends AppController
      */
     public function sounds()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $SoundsTable = $this->fetchTable("Sounds");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $sounds = $this->paginate($SoundsTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('sounds'));
     }
 
     /**
@@ -250,7 +479,22 @@ class AdminController extends AppController
      */
     public function state($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $StatesTable = $this->fetchTable("States");
+        $state = $StatesTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('state'));
     }
 
     /**
@@ -260,7 +504,26 @@ class AdminController extends AppController
      */
     public function states()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
 
+        $StatesTable = $this->fetchTable("States");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $states = $this->paginate($StatesTable);
+
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('states'));
     }
 
     /**
@@ -272,7 +535,22 @@ class AdminController extends AppController
      */
     public function user($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $UsersTable = $this->fetchTable("Users");
+        $user = $UsersTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('user'));
     }
 
     /**
@@ -282,7 +560,26 @@ class AdminController extends AppController
      */
     public function users()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $UsersTable = $this->fetchTable("Users");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $users = $this->paginate($UsersTable);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('users'));
     }
 
     /**
@@ -294,7 +591,22 @@ class AdminController extends AppController
      */
     public function zone($id = null)
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $ZonesTable = $this->fetchTable("Zones");
+        $zone = $ZonesTable->get($id, ['contain' => []]);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        }
+
+        $this->set(compact('zone'));
     }
 
     /**
@@ -304,6 +616,25 @@ class AdminController extends AppController
      */
     public function zones()
     {
+        $session = $this->request->getSession();
+        
+        if (!$session->check('Auth.User'))
+        {
+            return $this->redirect(['controller' => 'Account', 'action' => 'login']);
+        }
+        
+        $ZonesTable = $this->fetchTable("Zones");
+        $this->paginate = [
+            'maxLimit' => 512,
+            'limit'    => 32,
+        ];
+        $zones = $this->paginate($ZonesTable);
 
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
+            
+        } 
+
+        $this->set(compact('zones'));
     }
 }
